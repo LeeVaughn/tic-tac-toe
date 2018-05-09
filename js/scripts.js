0e12e4f24663b292aea11f213eebce5e51b3d6ce
Lee Vaughn <32204461+LeeVaughn@users.noreply.github.com>
2018-05-08 20:32:28 -0700

    Update scripts.js
    Added generic player name if player 1 input is left blank, created gamePlay function which shows game piece images when squares are hovered over, add class to clicked squares, and alternates between moves for the two players. Started working on endCheck function which will determine when the game has been won or drawn.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@
     let $startHTML;
     let $name1HTML;
     let $name2HTML;
+    const $box = $(".box");
+    let $xMoves;
+    let $0Moves;
 
     // creates start screen
     function startScreen() {
@@ -27,6 +30,10 @@
             // stores player name inputs
             $nameInput1 = $("#input1").val().toUpperCase();
             $nameInput2 = $("#input2").val().toUpperCase();
+            // adds generic player name to player 1 if left blank
+            if ($nameInput1 === "") {
+                $nameInput1 = "Player 1";
+            }
             // adds the name of computer to player 2 if left blank
             if ($nameInput2 === "") {
                 $nameInput2 = "COMPUTER";
@@ -39,6 +46,7 @@
             // calls functions necessary to begin game
             randomizePlayer();
             beginGame();
+            gamePlay();
         });
     }
 
@@ -61,8 +69,54 @@
         $board.show();
         $playerX.addClass("active");
     }
-    
+
+    // shows active player's game piece when an empty square is hovered over
     function gamePlay() {
+        $box.mouseover(function () {
+            if (!$(this).hasClass("box-filled-1") && !$(this).hasClass("box-filled-2")) {
+                if ($playerX.hasClass("active")) {
+                    $(this).css("background-image", "url('img/x.svg')");
+                }
+                if ($playerO.hasClass("active")) {
+                    $(this).css("background-image", "url('img/o.svg')");
+                }
+            }
+        });
+        $box.mouseout(function () {
+            if (!$(this).hasClass("box-filled-1") && !$(this).hasClass("box-filled-2")) {
+                if ($playerX.hasClass("active")) {
+                    $(this).css("background-image", "none");
+                }
+                if ($playerO.hasClass("active")) {
+                    $(this).css("background-image", "none");
+                }
+            }
+        });
+        $box.click(function () {
+            if (!$(this).hasClass("box-filled-1") && !$(this).hasClass("box-filled-2")) {
+                if ($playerX.hasClass("active")) {
+                    $(this).addClass("box-filled-2");
+                    oActive();
+                    console.log(this.id);
+                } else if ($playerO.hasClass("active")) {
+                    $(this).addClass("box-filled-1");
+                    xActive();
+                }
+            }
+        });
+    }
+
+    function xActive() {
+        $playerO.removeClass("active");
+        $playerX.addClass("active");
+    }
+
+    function oActive() {
+        $playerX.removeClass("active");
+        $playerO.addClass("active");
+    }
+
+    function endCheck() {
 
     }
 
